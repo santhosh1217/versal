536b13a571f2f581876c8cc828f3b42b606265fa
santhosh1217 <79699124+santhosh1217@users.noreply.github.com>
2022-12-18 18:55:23 +0530

    Update settings.py

--- park/settings.py
+++ park/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = 'django-insecure-@p-)#4_y$vtg0@7!gy8^yodcge8%wg#zweh*(_+h%e=@j58ao_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 
 # Application definition
